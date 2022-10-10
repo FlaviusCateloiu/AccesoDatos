7db31f0f0ef4286bc676c24b89548e6a857fb892
flaars <fcateloiu08@gmail.com>
2022-10-10 17:20:15 +0200

    Reestructuracion del repositorio.

--- Unidad1/Ejercicio04EscribirFicheros/Ejercicio4.4/Ejercicio4_4.java
+++ Unidad1/Ejercicio04EscribirFicheros/Ejercicio4.4/Ejercicio4_4.java
@@ -1,9 +1,9 @@
 public class Ejercicio4_4 {
     public static void main(String[] args) {
-        Product producto = new Product(1, "Pepe", 3, 4, 3.45, 7);
+        Product producto = new Product(4, "Madera", 2, 6, 8.32, 18);
         String ruta = "Unidad1/Ejercicio04EscribirFicheros/Ejercicio4.4/productos2_0.csv";
         producto.writeFile(ruta);
-        producto = new Product(2, "Losas", 23, 4, 6.45, 22);
+        producto = new Product(3, "Mangueras", 243, 3, 2.67, 4);
         producto.writeFile(ruta);
     }
 }
---
+++ Unidad1/Ejercicio06RandomAccessFile/Ejercicio6.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        String nameProduct, supplierProduct;
+        String ruta = "Unidad1/Ejercicio06RandomAccessFile/productos2_0.csv";
+
+    }
+}
