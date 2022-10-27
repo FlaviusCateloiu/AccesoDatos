f97601a9e0631bb0324ead550898cb616d47702b
flaars <fcateloiu08@gmail.com>
2022-10-27 16:00:29 +0200

    Creacion de la tarea

--- Unidad1/Ejercicio08TratamientoXML/Race.java
+++ Unidad1/Ejercicio08TratamientoXML/Race.java
@@ -1,4 +1,9 @@
 package Ejercicio08TratamientoXML;
 
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+@XmlRootElement (name = "")
+@XmlType()
 public class Race {
 }
